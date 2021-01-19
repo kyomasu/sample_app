06e49fcf3a4aef7b711c8a354129b0681441475c
EC2 Default User <ec2-user@ip-172-31-24-160.ec2.internal>
2021-01-19 13:32:09 +0000

    [Update]2-7

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '65bbacc9dad43d32dcd8668fbbcac8c61a816924744677f8e5339e3234baf1d3c3266559e88ad87f372bbaede0d0d48ca5af176c5e8e31c4194d7a92c7e5ccec'
